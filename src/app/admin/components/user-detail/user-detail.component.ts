90c0adeae5fdcc4054649484741f43f9183561cb
Sedky <sedkyaboushamalah@gmail.com>
2020-03-21 19:38:57 -0400

    upgrade to Angular 9

--- src/app/admin/components/user-detail/user-detail.component.ts
+++ src/app/admin/components/user-detail/user-detail.component.ts
@@ -26,6 +26,7 @@ export class UserDetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
   closeDetails() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  firebase: {}
+  firebase: {
+    apiKey: "AIzaSyAQVu7JXpG-u5LDYeltF8DyOpn-agZaJp8",
+    authDomain: "bloopky.firebaseapp.com",
+    databaseURL: "https://bloopky.firebaseio.com",
+    projectId: "bloopky",
+    storageBucket: "bloopky.appspot.com",
+    messagingSenderId: "849578959436",
+    appId: "1:849578959436:web:b8f8144560a29629d48ae9",
+    measurementId: "G-BX1MCQ199J"
+  }
 };
 
 /*
